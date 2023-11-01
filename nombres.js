4900e50f253fc3583fb47f4a2b803a891a7275f7
amadoucodeur <amadoucodeur@gmail.com>
2023-11-01 13:15:35 +0000

    le playground

--- nombres.js
+++ nombres.js
@@ -13,11 +13,10 @@ const str = "hello"
 // console.log("  aab  ".includes("x"));
 // console.log("  aab  ".repeat(3));
 // console.log("  aab  ".replaceAll("aab", "x"));
-// console.log("  aab  ".replaceAll("aab", "x"));
 // console.log("  AAB  ".toLocaleLowerCase());
 // console.log("  aab  ".toUpperCase());
-// console.log("  aab  ".substring(2, 6));
 // console.log("  aab  ".slice(2, 6));
+// console.log("  aab  ".substring(2, 6));
 // console.log(" , a,a,b , ".split(","));
 
 
---
+++ tableaux.js
@@ -0,0 +1,63 @@
+const t = [1, 2, 3, 4];
+const tt = new Array(2, 3, 4);
+const ttt = Array.of(10, 20, 30);
+
+const odj = { a: "aaa", b: "bbb" };
+const odjArray = Array.from(odj);
+// console.log(Array.isArray(t))
+// console.log(t.at(1))
+
+// for(let i = 0; i <= t.length-1; i++){
+//     console.log(t[i])
+// }
+
+// for( value of t){
+//     console.log(value)
+// }
+
+// for (index in t) {
+//   console.log(index);
+// }
+
+// t.forEach((value, index) => {
+//     console.log(index, " => ", value)
+// });
+
+// const str = "salut a tous"
+// const arr = str.split(" ")
+// console.log(arr)
+// const str2 = arr.join(",")
+// console.log(str2)
+
+// t.push(4,5)
+// t.unshift(-1,0)
+// console.log(t)
+
+// console.log(t.pop())
+// console.log(t.shift())
+// console.log(t)
+
+// console.log(t.indexOf(3))
+// console.log(t.lastIndexOf(1))
+// console.log(t.includes(2))
+
+// const tf = t.find(el => el === 2)
+// console.log(tf)
+
+// const tfo = t.findIndex((el) => el === 2);
+// console.log(tfo);
+
+// const tc = t.concat([4,5,6])
+// console.log(tc)
+
+// const ts = [10, -1,14, 0,0]
+// console.log(ts.sort())
+// console.log(ts.reverse())
+
+// console.log(t.slice(0, 2))
+
+// const tf = t.filter( el => el%2 == 0)
+// console.log(tf)
+
+// const tm = t.map(el => el**2 )
+// console.log(tm)
